46e9e3cfc778ceb79dd1001b23847c56f26a374d
lcberg <50460080+lcberg@users.noreply.github.com>
2021-04-22 23:50:06 +0200

    refractor old code

--- src/index.ts
+++ src/index.ts
@@ -108,33 +108,3 @@ async function isZehner(elem: playwright.ElementHandle<HTMLElement | SVGElement>
     }
     return false;
 }
-    
-
-async function makeScreenshot(name: string) {
-    console.log('running');
-
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
-
-    const urlName = name.replaceAll(' ', '+');
-
-    await page.goto(`https://euw.op.gg/summoner/userName=${urlName}`);
-    await page.click('button:has-text("ZUSTIMMEN")');
-    await page.click('#SummonerRefreshButton');
-
-    await sleep(3000);
-    
-    await page.click('.Button.MatchDetail .Off');
-    
-    await sleep(2000);
-    const element = await page.$(`td:has-text("${name}")`);
-    const example_parent = (await element?.$('xpath=..'));
-    const example_grandparent = (await element?.$('xpath=../..'));
-    const data = await example_parent?.screenshot({ path: 'row.png', omitBackground: true });
-    await example_grandparent?.screenshot({ path: 'table.png', omitBackground: true });
-
-    console.log('done');
-    
-    await browser.close();
-    return data;
-}
