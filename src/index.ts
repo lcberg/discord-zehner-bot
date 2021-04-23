9d8818c06b405ebb40ed3e67b16468783933cce6
lcberg <50460080+lcberg@users.noreply.github.com>
2021-04-23 10:06:04 +0200

    fix memory leak for good and prevent browser from closing early

--- src/index.ts
+++ src/index.ts
@@ -5,50 +5,79 @@ import dotenv from 'dotenv';
 const send = true;
 dotenv.config();
 
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('client ready');
-})
-
-client.on('message', async message => {
-    if (message.channel.id === process.env.CHANNEL_ID && message.content.startsWith('!zehner')) {
-        console.log(`${message.author.username} has requested a zehner screenshot: ${message.content}`);
-        
-        if (send) message.reply('Moment ich schaue nach...');
-
-        const name = message.content.replace('!zehner', '').trim();
-
-        if (name) {
-            const elem = await getTableElement(name);
-            if (!elem) return;
-            await elem.screenshot({ path: 'row.png', omitBackground: true });
-
-            const score = await getScore(elem);
-            const zehner = await isZehner(elem);
-
-            if (send && !zehner) {
-                console.log('Wasnt zehner... sending trump');
-
-                message.reply('Nee Atze...', {
-                    files: [
-                        './wrong.jpg'
-                    ]
-                });
-            } else if (send) {
-                console.log('was zehner... sending score');
-                message.reply('Ergebnis:', {
-                    files: [
-                        "./row.png"
-                    ]
-                });
+async function init () {
+    const client = new Discord.Client();
+    let browser = await chromium.launch({ chromiumSandbox: false });
+    
+    client.on('ready', () => {
+        console.log('client ready');
+    })
+    
+    client.on('message', async message => {
+        if (message.channel.id === process.env.CHANNEL_ID && message.content.startsWith('!zehner')) {
+            console.log(`${message.author.username} has requested a zehner screenshot: ${message.content}`);
+            
+            if (send) message.reply('Moment ich schaue nach...');
+    
+            const name = message.content.replace('!zehner', '').trim();
+    
+            if (name) {
+                const elem = await getTableElement(name);
+                if (!elem) return;
+                await elem.screenshot({ path: 'row.png', omitBackground: true });
+    
+                const score = await getScore(elem);
+                const zehner = await isZehner(elem);
+    
+                if (send && !zehner) {
+                    console.log('Wasnt zehner... sending trump');
+    
+                    message.reply('Nee Atze...', {
+                        files: [
+                            './wrong.jpg'
+                        ]
+                    });
+                } else if (send) {
+                    console.log('was zehner... sending score');
+                    message.reply('Ergebnis:', {
+                        files: [
+                            "./row.png"
+                        ]
+                    });
+                }
             }
         }
+    });
+    
+    async function getTableElement(name: string): Promise<playwright.ElementHandle<HTMLElement | SVGElement> | null> {
+        try {
+            console.log('Fetching element...');
+        
+            const page = await browser.newPage({ extraHTTPHeaders: { 'Accept-Language': 'de-De' }});
+            await page.goto(`https://euw.op.gg/summoner/userName=${name}`);
+            await page.click('button:has-text("ZUSTIMMEN")');
+            await page.click('#SummonerRefreshButton');
+        
+            await sleep(3000);
+        
+            await page.click('.Button.MatchDetail .Off');
+        
+            await sleep(2000);
+    
+            const element = await page.$(`td:has-text("${name}")`);
+            const example_parent = (await element?.$('xpath=..'));
+            
+            console.log('Done getting the element');
+            
+            return example_parent!;
+        } catch(error) {
+            console.log(error);
+            return null;
+        }
     }
-});
 
-client.login(process.env.DISCORD_TOKEN);
-
+    client.login(process.env.DISCORD_TOKEN);
+}
 
 function sleep (ms: number): Promise<void> {
     console.log(`waiting ${ms} ms`);
@@ -60,35 +89,6 @@ function sleep (ms: number): Promise<void> {
     });
 }
 
-async function getTableElement(name: string): Promise<playwright.ElementHandle<HTMLElement | SVGElement> | null> {
-    try {
-        console.log('Fetching element...');
-    
-        const browser = await chromium.launch({ chromiumSandbox: false });
-        const page = await browser.newPage({ extraHTTPHeaders: { 'Accept-Language': 'de-De' }});
-        await page.goto(`https://euw.op.gg/summoner/userName=${name}`);
-        await page.click('button:has-text("ZUSTIMMEN")');
-        await page.click('#SummonerRefreshButton');
-    
-        await sleep(3000);
-    
-        await page.click('.Button.MatchDetail .Off');
-    
-        await sleep(2000);
-
-        const element = await page.$(`td:has-text("${name}")`);
-        const example_parent = (await element?.$('xpath=..'));
-        
-        console.log('Done getting the element');
-
-        await browser.close();
-    
-        return example_parent!;
-    } catch(error) {
-        console.log(error);
-        return null;
-    }
-}
 
 async function getScore(elem: playwright.ElementHandle<HTMLElement | SVGElement>): Promise<string> {
     const scoreElem = await (elem.$$('.OPScore.Text'));
