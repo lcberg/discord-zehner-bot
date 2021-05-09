05f833171ab1976b3243c6ac06914c2e627a9ed0
lcberg <50460080+lcberg@users.noreply.github.com>
2021-05-09 12:50:43 +0200

    champion and damage persisted to database

--- src/database/repositories/OpScoreRepository.ts
+++ src/database/repositories/OpScoreRepository.ts
@@ -4,12 +4,12 @@ import { GameStatsRepository } from "./GameStatsRepository";
 
 @EntityRepository(OpScore)
 export class OpScoreRepository extends Repository<OpScore> {
-    async createAndSave(gameId: string, name: string, score: number, place: number) {
+    async createAndSave(gameId: string, name: string, score: number, place: number, champion: string, damage: number) {
         const gameRepo = getCustomRepository(GameStatsRepository);
         const game = await gameRepo.findOne({ id: gameId });
 
         if (game) {
-            const opScore = new OpScore(game, name, score, place);
+            const opScore = new OpScore(game, name, score, place, champion, damage);
             return this.manager.save(opScore);
         }
     }
--- src/models/OpScore.ts
+++ src/models/OpScore.ts
@@ -1,3 +1,4 @@
+import { timingSafeEqual } from "crypto";
 import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { GameStats } from "./GameStats";
 
@@ -16,13 +17,21 @@ export class OpScore {
     @Column("decimal")
     score!: number;
 
+    @Column("text")
+    champion!: string;
+
+    @Column("decimal")
+    damage!: number;
+
     @Column("int")
     place!: number;
 
-    constructor(gameStats?: GameStats, name?: string, score?: number, place?: number) {
+    constructor(gameStats?: GameStats, name?: string, score?: number, place?: number, champion?: string, damage?: number) {
         if (gameStats) this.gameStats = gameStats;
         if (name) this.name = name;
         if (score) this.score =  score;
         if (place) this.place = place;
+        if (champion) this.champion = champion;
+        if (damage) this.damage = damage;
     }
 }
